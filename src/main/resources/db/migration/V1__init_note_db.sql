342d35d83cd88b8fd0340d0d66aab7470d6c76dc
AndreyPinchuk <44933416+AndreyPinchuk@users.noreply.github.com>
2023-02-22 02:58:10 +0200

    Added restrictions for title and context to the database

--- src/main/resources/db/migration/V1__init_note_db.sql
+++ src/main/resources/db/migration/V1__init_note_db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE note (
     id IDENTITY PRIMARY KEY,
-    title VARCHAR (1000),
-    content VARCHAR (1000)
+    title VARCHAR (100) CHECK (LENGTH(title) >= 5) NOT NULL,
+    content VARCHAR (10000) CHECK (LENGTH(content) >= 5) NOT NULL
 );
